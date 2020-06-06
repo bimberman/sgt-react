adef8706d07b68849a43f4acd2ae462e8185b990
Ben Imberman <bimberman@gmail.com>
2020-06-06 15:34:22 -0700

    Fixed the side-effect in the getAvgGrade method

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 export default class Header extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
   render() {
     return (
       <header className="container-fluid w-75 mt-5 mb-4" id="header">
-        <h1>
-        Student Grade Table
-        </h1>
+        <div className="row d-flex justify-content-around">
+          <h1>
+            Student Grade Table
+          </h1>
+          <h3 className="text-center">
+            Average Grade
+            <span className="badge badge-secondary ml-3">{this.props.avg}</span>
+          </h3>
+        </div>
       </header>
     );
   }
